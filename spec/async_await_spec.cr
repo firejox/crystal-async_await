6d8b3c425e7f901b94ffb5de01360673c1cd7fc5
firejox <firejox@gmail.com>
2017-01-06 02:23:20 +0800

    update general spec

--- spec/async_await_spec.cr
+++ spec/async_await_spec.cr
@@ -6,6 +6,16 @@ private async def foo
   Time.now - st
 end
 
+private class Foo
+  getter ch = AAChannel(String).new
+
+  async def bar
+    await Task.delay(Time::Span.new(0, 0, 1))
+    await ch.receive
+    await ch.send "Crystal!"
+  end
+end
+
 describe AsyncAwait do
   it "async/await work" do
     task = uninitialized Task(Time::Span?)
@@ -14,5 +24,16 @@ describe AsyncAwait do
     }
     a.join
     task.value.not_nil!.should be >= Time::Span.new(0, 0, 1)
+  end
+
+  it "work with channels" do
+    a = Foo.new
+
+    async_spawn do
+      a.bar
+    end
+
+    a.ch.send_with_csp "Hello"
+    a.ch.receive_with_csp.should eq("Crystal!")
   end
 end
