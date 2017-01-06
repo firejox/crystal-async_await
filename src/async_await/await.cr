d7f1adc3e6720504a519e96086ba3073d18f4c1b
firejox <firejox@gmail.com>
2017-01-06 23:31:12 +0800

    remove the limitaion of await use

--- src/async_await/await.cr
+++ src/async_await/await.cr
@@ -1,19 +1,15 @@
 macro await(call)
-  {% if call.is_a?(Call) %}
-    {{call}}.try do |%awaitee|
-      AsyncAwait.current_call.try do |%current|
-        %current.awaitee = ->%awaitee.status
-        %current.set_current_ip
+  {{call}}.try do |%awaitee|
+    AsyncAwait.current_call.try do |%current|
+      %current.awaitee = ->%awaitee.status
+      %current.set_current_ip
 
-        if %awaitee.status == AAStatus::INCOMPLETE
-          %current.dump_stack
-          return nil
-        end
-        AsyncAwait.current_call.try &.clean
+      if %awaitee.status == AAStatus::INCOMPLETE
+        %current.dump_stack
+        return nil
       end
-      %awaitee.value.itself
+      AsyncAwait.current_call.try &.clean
     end
-  {% else %}
-    {{ raise "Unsupported expression for await" }}
-  {% end %}
+    %awaitee.value.itself
+  end
 end
